a98037e98c7a11489a5d10be892853c6c3104be9
Andrija Cenic <andrija.cenic@elfak.rs>
2022-10-26 19:43:34 +0200

    4

---
+++ Lab1/4.py
@@ -0,0 +1,14 @@
+def zbir(l):
+    return list(filter(lambda y: type(y) in (int, float), map(lambda x: x[1] + l[x[0] + 1] if x[0]+1 < len(l) else (), list(enumerate(l)))))
+
+
+def suma(l, i=0):
+    if type(l[i]) in (int, float):
+        return l[i] + (suma(l, i+1) if len(l) > i+1 else 0)
+    elif type(l[i]) is list:
+        return suma(l[i], 0) + (suma(l, i+1) if len(l) > i+1 else 0)
+
+
+print(zbir([1, 2, 3, 4, 5]))
+
+print(suma([1, 1, [1, [1, [1, [[1]]]]], 1, [1, 1, [1, 1]]]))
