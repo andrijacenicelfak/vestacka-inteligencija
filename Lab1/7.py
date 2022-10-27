2d934ab10d03fb8d10d03b9549ba55c18039b7ff
Andrija Cenic <andrija.cenic@elfak.rs>
2022-10-27 17:20:14 +0200

    7 - 7b ispravka

--- Lab1/7.py
+++ Lab1/7.py
@@ -5,8 +5,14 @@ def operacija(l1, func):
     return list(map(lambda t: reduce(func, t), l1))
 
 
+def dodajnaprvo(a: dict, b: dict):
+    a.update(b)
+    return a
+
+
 def objedini(l1):
-    return list(map(lambda x: {x: None} if type(x) in (int, float) else {x[0]: list(map(lambda z: z[1], filter(lambda y: y[0] > 0, enumerate(list(x)))))}, l1))
+    return reduce(lambda a, b: dodajnaprvo(a, b), list(map(lambda x: {x: None} if type(x) in (int, float) else {x[0]: list(
+        map(lambda z: z[1], filter(lambda y: y[0] > 0, enumerate(list(x)))))}, l1)))
 
 
 print(operacija([(1, 4, 6), (2, 4), (4, 1)], lambda x, y: x + y))
