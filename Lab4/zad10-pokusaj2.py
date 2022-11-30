865431b046a2415c1afa9d64aeb7698183ba8827
Andrija Cenic <andrija.cenic@elfak.rs>
2022-11-30 23:38:15 +0100

    statistika

--- Lab4/zad10-pokusaj2.py
+++ Lab4/zad10-pokusaj2.py
@@ -58,7 +58,7 @@ def make_tuple(tabla):
 def make_list(tabla):
     return reduce(lambda a, b: [*a, reduce(lambda x, y: [*x, y], b, tuple())], tabla, tuple())
 
-def pronadji_put(tabla, domine):
+def pronadji_put(tabla, domine, use_heuristics=True):
     broj_proverenih_stanja = 0
     open_set = list()
     kraj = False
@@ -75,13 +75,21 @@ def pronadji_put(tabla, domine):
             (bp, nt, ds, k) = open_set[i]
             if(len(ds) == 0):
                 continue
-            trenutno = ima_u_tabli(nt, ds[0])
-            trenutna_domina = ds[0]
-            for j in ds[1:]:
-                t2 = ima_u_tabli(nt, j)
-                if trenutno[0] > t2[0]:
-                    trenutno = t2
-                    trenutna_domina = j
+            trenutno = None
+            trenutna_domina = None
+            if use_heuristics:
+                trenutno = ima_u_tabli(nt, ds[0])
+                trenutna_domina = ds[0]
+
+                for j in ds[1:]:
+                    t2 = ima_u_tabli(nt, j)
+                    if trenutno[0] > t2[0]:
+                        trenutno = t2
+                        trenutna_domina = j
+            else:
+                trenutna_domina = ds[0]
+                trenutno = ima_u_tabli(nt, trenutna_domina)
+
             trenutne_domine = list(filter(lambda a: a != trenutna_domina, ds))
             if nova_tabla is None or trenutno[0] < heur:
                 domina = trenutna_domina
@@ -103,54 +111,85 @@ def pronadji_put(tabla, domine):
                 svi_koraci = novi_koraci
     return (svi_koraci, broj_proverenih_stanja)
 
-tabla = [
-    [2, 3, 2, 2, 2],
-    [3, 0, 3, 0, 0],
-    [3, 0, 3, 1, 1],
-    [0, 1, 1, 1, 2]
-]
-tabla2 = [
-    [2, 3, 3, 0, 0],
-    [2, 1, 3, 1, 0],
-    [2, 0, 2, 1, 0],
-    [3, 3, 1, 2, 1]
-]
-tabla3 = [
-    [2, 3, 3, 0, 0],
-    [2, 1, 3, 0, 0],
-    [2, 1, 1, 2, 0],
-    [3, 3, 1, 2, 1]
-]
-tabla4 = [[2, 3, 2, 2, 2],
-          [3, 0, 0, 3, 0],
-          [3, 0, 3, 1, 1],
-          [0, 1, 1, 1, 2]]
 
+def stampaj_tablu_po_koracima(koraci, broj_prolaza):
+    poredjana_tabla = [[('_', False)]*5 for i in range(4)]
+
+    for korak in koraci:
+        kord = korak[1][0]
+        domina = korak[0]
+        poredjana_tabla[kord[0]][kord[1]] = (domina[0], True)
+        if korak[1][1]:
+            poredjana_tabla[kord[0]+1][kord[1]] = (domina[1], True)
+        else:
+            poredjana_tabla[kord[0]][kord[1]+1] = (domina[1], True)
+        for i in range(len(poredjana_tabla)):
+            for j in range(len(poredjana_tabla[0])):
+                if poredjana_tabla[i][j][1]:
+                    cprint(poredjana_tabla[i][j][0],
+                           color="blue", attrs=['bold'], end=' ')
+                else:
+                    print(poredjana_tabla[i][j][0], end=' ')
+                poredjana_tabla[i][j] = (poredjana_tabla[i][j][0], False)
+            print('\n', end='')
+        print('\n', end='')
+    print(koraci)
+    print("Broj stanja : ", broj_prolaza)
+
+tabele = list()
+
+tabele.append([
+            [2, 3, 2, 2, 2],
+            [3, 0, 3, 0, 0],
+            [3, 0, 3, 1, 1],
+            [0, 1, 1, 1, 2]
+            ])
+tabele.append([
+            [2, 3, 3, 0, 0],
+            [2, 1, 3, 1, 0],
+            [2, 0, 2, 1, 0],
+            [3, 3, 1, 2, 1]
+            ])
+tabele.append([
+            [2, 3, 3, 0, 0],
+            [2, 1, 3, 0, 0],
+            [2, 1, 1, 2, 0],
+            [3, 3, 1, 2, 1]
+            ])
+tabele.append([
+            [2, 3, 2, 2, 2],
+            [3, 0, 0, 3, 0],
+            [3, 0, 3, 1, 1],
+            [0, 1, 1, 1, 2]
+            ])
+
+tabele.append([
+            [3, 0, 1, 3, 0],
+            [3, 2, 1, 0, 1],
+            [2, 2, 3, 1, 0],
+            [1, 2, 2, 3, 0]
+            ])
 domine = [
     (3, 3), (3, 2), (3, 1), (3, 0),
     (2, 2), (2, 1), (2, 0),
     (1, 1), (1, 0),
     (0, 0)
 ]
-(koraci, broj_prolaza) = pronadji_put(tabla4, domine)
-poredjana_tabla = [[('_', False)]*len(tabla[0]) for i in range(len(tabla))]
+(koraci, broj_prolaza) = pronadji_put(tabele[0], domine)
+stampaj_tablu_po_koracima(koraci, broj_prolaza)
 
-for korak in koraci:
-    kord = korak[1][0]
-    domina = korak[0]
-    poredjana_tabla[kord[0]][kord[1]] = (domina[0], True)
-    if korak[1][1] :
-        poredjana_tabla[kord[0]+1][kord[1]] = (domina[1], True)
-    else:
-        poredjana_tabla[kord[0]][kord[1]+1]= (domina[1], True)
-    for i in range(len(poredjana_tabla)):
-        for j in range(len(poredjana_tabla[0])):
-            if poredjana_tabla[i][j][1]:
-                cprint(poredjana_tabla[i][j][0],color="blue", attrs=['bold'] ,end=' ')
-            else:
-                print(poredjana_tabla[i][j][0], end=' ')
-            poredjana_tabla[i][j] = (poredjana_tabla[i][j][0], False)
-        print('\n', end='')
-    print('\n', end='')
-print(koraci)
-print("Broj stanja : ", broj_prolaza)
+ukupno_prolaza = 0
+
+for tabela in tabele:
+    (koraci, broj_prolaza) = pronadji_put(tabela, domine)
+    ukupno_prolaza += broj_prolaza
+
+print("Sa heuristikom srednji broj predjenih stanja : ", int(ukupno_prolaza/len(tabele)))
+
+ukupno_prolaza = 0
+
+for tabela in tabele:
+    (koraci, broj_prolaza) = pronadji_put(tabela, domine, False)
+    ukupno_prolaza += broj_prolaza
+
+print("Bez heuristikom srednji broj predjenih stanja : ", int(ukupno_prolaza/len(tabele)))
